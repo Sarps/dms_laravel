f4a8f408bdd43e6d47cbfdf85339ab379d32d123
Emmanuel Oppong-Sarpong <esarpong51@gmail.com>
2019-12-28 08:29:28 +0000

    Initial Management Commit

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
@@ -11,6 +12,17 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        User::create([
+                'name' => 'Emmanuel Sarpong',
+                'email' => 'esarpong51@gmail.com',
+                'password' => Hash::make('secret')
+        ]);
+
+//        User::create([
+//            'name' => 'Emmanuel Sarpong',
+//            'email' => 'esarpong51@gmail.com',
+//            'password' => Hash::make('secret')
+//        ]);
+
     }
 }
